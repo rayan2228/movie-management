24517d61aa72d5689c8b4e28a0e4c80775ee78cc
rayan2228 <rayanh.dev@gmail.com>
2025-01-22 14:21:29 +0600

    feat: implement permission management; add create and get permissions functionality

---
+++ src/controllers/permission.controller.js
@@ -0,0 +1,33 @@
+import { Permission } from "../models/permissionSchema.model";
+import ApiResponse from "../utils/ApiResponse";
+import { TryCatch } from "../utils/TryCatch";
+
+const createPermission = TryCatch(async (req, res) => {
+  // Code to create a permission
+  const { name, description } = req.body;
+  // Check if required fields are missing
+  if (!name) {
+    throw new ApiError(400, "name is required");
+  }
+  // Check if permission already exists
+  const permission = await Permission.findOne({ name });
+  if (permission) {
+    throw new ApiError(400, "Permission already exists");
+  }
+  // Create a new permission
+  const newPermission = await Permission.create({ name, description });
+  return res
+    .status(201)
+    .json(new ApiResponse(201, "Permission created", newPermission));
+});
+
+const getPermissions = TryCatch(async (req, res) => {
+  // Code to get all permissions
+  const permissions = await Permission.find();
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "All permissions", permissions));
+});
+
+export { createPermission, getPermissions };
+
