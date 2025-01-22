246cd5986670844ea29281298efc731ccfabd89c
rayan2228 <rayanh.dev@gmail.com>
2025-01-22 11:56:19 +0600

    refactor: update env and mongodb connection

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
+import {
+    MONGODB_DB_NAME,
+    MONGODB_PASSWORD,
+    MONGODB_USERNAME,
+} from "../constants.js";
 import { TryCatch } from "../utils/TryCatch.js";
-import { MONGODB_URL } from "../constants.js";
 
 const mongoConnect = TryCatch(async () => {
-    await mongoose.connect(MONGODB_URL);
-    console.log("Connected to MongoDB");
+  await mongoose.connect(
+    `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.fb9gw.mongodb.net/${MONGODB_DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+  );
+  console.log("Connected to MongoDB");
 });
 
 export default mongoConnect;
