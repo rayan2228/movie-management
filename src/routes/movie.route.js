f91a460826c8e87142b89bee69654e2c0daf06f8
rayan2228 <rayanh.dev@gmail.com>
2025-01-23 15:10:44 +0600

    feat: update movie routes to use a details endpoint for fetching and updating movie information

--- src/routes/movie.route.js
+++ src/routes/movie.route.js
@@ -10,6 +10,6 @@ import { auth } from "../middlewares/auth.middleware.js";
 const router = e.Router();
 
 router.route("/movies").get(getMovies).post(auth, createMovie);
-router.route("/movies/:id").get(auth, getMovie).put(auth, updateMovie);
 router.route("/movies/users").get(auth, getMoviesByUser);
+router.route("/movies/details/:id").get(auth, getMovie).put(auth, updateMovie);
 export default router;
