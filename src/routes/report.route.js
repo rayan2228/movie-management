52d2234758c6f5379dd51aeb16a51d7b28a9774a
rayan2228 <rayanh.dev@gmail.com>
2025-01-23 12:52:55 +0600

    feat: reorganize report routes; separate movie report creation and update endpoints

--- src/routes/report.route.js
+++ src/routes/report.route.js
@@ -7,9 +7,9 @@ import {
 import { auth } from "../middlewares/auth.middleware.js";
 import { checkAccess } from "../middlewares/checkAccess.middleware.js";
 const router = e.Router();
+router.route("/reports").get(auth, checkAccess(["admin"]), getReports);
 router
-  .route("/reports")
-  .get(auth, checkAccess(["admin"]), getReports)
-  .put(auth, checkAccess(["admin"]), updateReport)
-  .post(auth, createReport);
+  .route("/reports/movies/:id")
+  .post(auth, createReport)
+  .put(auth, checkAccess(["admin"]), updateReport);
 export default router;
